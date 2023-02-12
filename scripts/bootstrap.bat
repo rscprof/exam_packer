6a0635082c2db566dee68ffdba4c6869b12a16e0
Glusker Alexander <rsc@rsc.pp.ru>
2023-02-12 18:09:58 +0300

    Added js frameworks, fix some issues

--- scripts/bootstrap.bat
+++ scripts/bootstrap.bat
@@ -1,5 +1,4 @@
 rem download bootstrap
 if not exist "C:\User\Student\Desktop\bootstrap.zip" (
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/twbs/bootstrap/releases/download/v4.0.0/bootstrap-4.0.0-dist.zip
-', 'C:\User\Student\Desktop\bootstrap.zip')" <NUL
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/twbs/bootstrap/releases/download/v4.0.0/bootstrap-4.0.0-dist.zip', 'C:\User\Student\Desktop\bootstrap.zip')" <NUL
 )
--- scripts/install_pm5.bat
+++ scripts/install_pm5.bat
@@ -2,14 +2,15 @@ set PATH=%PATH%;C:\ProgramData\chocolatey\
 if not exist "C:\Windows\Temp\laragon-wamp.exe" (
   powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/leokhoa/laragon/releases/download/6.0.0/laragon-wamp.exe', 'C:\Windows\Temp\laragon-wamp.exe')" <NUL
 )
-start C:\Windows\temp\laragon-wamp.exe /SILENT
+start /WAIT C:\Windows\temp\laragon-wamp.exe /SILENT
 choco install /y vscode
 choco install /y gimp
-choco install /y impress
+choco install /y libreoffice-fresh
 choco install /y postman
-choco install /y pycharm
-start c:\laragon\bin\nodejs\node-v18\npm install -g express prettier jslint
-start c:\laragon\bin\python\python-3.10\python -m pip install flask flask-sqlalchemy jinja2 flask-mysql secrets
-if not exist "C:\laragon\tmp\cached\wordpress.latest.tar.g" (
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://wordpress.org/latest.tar.gz', 'C:\laragon\tmp\cached\wordpress.latest.tar.gz')" <NUL
-)
+choco install /y pycharm-community
+choco install /y openssl
+call c:\laragon\bin\nodejs\node-v18\npm install -g express prettier jslint vue@next vue-router@4 react 
+c:\laragon\bin\python\python-3.10\python -m pip install flask flask-sqlalchemy jinja2 flask-mysql pyOpenSSL secrets
+mkdir c:\laragon\tmp\cached
+c:\laragon\bin\git\mingw64\bin\curl https://wordpress.org/latest.tar.gz --output C:\laragon\tmp\cached\wordpress.latest.tar.gz
+
